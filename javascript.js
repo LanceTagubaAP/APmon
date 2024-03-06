4797401b8dacc139f05f1f340d4a732828d615fa
Edison Tsang <s115239@ap.be>
2024-03-06 13:06:23 +0000

    javascript.js aanmaken

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function preLoad() {
+    a1 = new Image; a1.src = 'images/pitAuto.jpg';  
+    a2 = new Image; a2.src = 'images/pitJpgWb.jpg';
+    a3 = new Image; a3.src = 'images/pitJpgWbFinal.jpg';
+  }
