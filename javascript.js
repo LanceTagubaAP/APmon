5ff9acabcde80b2b420976b0d307433cd2f8156f
abe verschueren <153118736+AbeVerschueren@users.noreply.github.com>
2024-03-19 23:12:25 +0100

    changed style

--- javascript.js
+++ javascript.js
@@ -3,3 +3,21 @@ function preLoad() {
     a2 = new Image; a2.src = 'images/pitJpgWb.jpg';
     a3 = new Image; a3.src = 'images/pitJpgWbFinal.jpg';
   }
+
+
+  function showDidYouKnow(){
+    const img = document.querySelector('img');
+    const weetje = document.querySelector('#weetje');
+
+   
+    img.addEventListener('mouseover', function() {
+        weetje.style.display = 'block';
+    });
+
+    
+    weetje.addEventListener('mouseout', function() {
+        weetje.style.display = 'none';
+    });
+
+  }
+ 
