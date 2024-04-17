8e465d9e75bd94a19e4a08c9d9cf3ce944b61644
Taguba Lance (s143847) <lance.taguba@student.ap.be>
2024-04-17 08:20:07 +0000

    interfaces

---
+++ interfaces.ts
@@ -0,0 +1,26 @@
+enum Rank {
+    Beginner,
+    Trainer,
+    Expert,
+    Kampioen,
+    Legende
+}
+export interface Pokemon{
+    id : number
+    name : string
+    type : string[]
+    health : number
+    attack : number
+    defense : number
+    description : string
+    isCatched : boolean
+    sprite : string
+}
+export interface User{
+    id : number
+    userName : string
+    password : string
+    userPokemons : Pokemon[]
+    rank : Rank
+    icon : string
+}
