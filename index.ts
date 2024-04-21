0a8e1d0ac08032759919a8f9f70a51d4d8ec4c6d
Taguba Lance (s143847) <lance.taguba@student.ap.be>
2024-04-21 19:48:37 +0000

    expressjs template in index.ts

---
+++ index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+const app = express();
+
+app.set("port", 3000);
+
+
+app.get("/",(req,res)=>{
+    /**Hier komt eerste pagina */
+});
+
+app.get("/mainpage",(req,res)=>{
+    /**Hier komt menu pagina */
+});
+app.get("/fightchoose",(req,res)=>{
+    /**Hier komt pokemon vechten pagina */
+});
+
+app.get("/pokedex",(req,res)=>{
+    /**Hier komt pokedex pagina */
+});
+
+app.get("/whosthatpokemon",(req,res)=>{
+    /**Hier komt Who's that pokemon pagina */
+});
+
+app.get("/howtoplay",(req,res)=>{
+    /**Hier komt how to play pagina */
+});
+
+app.listen(app.get("port"), () =>
+    console.log("[server] http://localhost:" + app.get("port"))
+  );
